f3677d0ead6363fec1c9461e95459e5520f496d7
No1IrishStig <39631899+No1IrishStig@users.noreply.github.com>
2019-08-03 05:38:12 +0100

    Bug

--- cogs/economy.py
+++ cogs/economy.py
@@ -68,8 +68,10 @@ class Economy(commands.Cog):
         author = ctx.author
         if user is None:
             ctx.send(embed=lib.Editable("Uh oh", "Please mention a user to transfer to.", "Devo Bank"))
+            return
         if amount is None:
             ctx.send(embed=lib.Editable("Oops", "Please specify an amount of credits to set.", "Devo Bank"))
+            return
         if author == user:
             await ctx.send(f"{ctx.author}, Bitch, do I look like a joke to you?")
             return
@@ -90,8 +92,10 @@ class Economy(commands.Cog):
     async def set(self, ctx, user: discord.Member=None, amount : int=None):
         if user is None:
             ctx.send(embed=lib.Editable("Uh oh", "Please mention a users bank to set.", "Devo Bank"))
+            return
         if amount is None:
             ctx.send(embed=lib.Editable("Oops", "Please specify an amount of credits to set.", "Devo Bank"))
+            return
         if ctx.author.id in self.config.owner:
             done = self.set_money(user.id, amount)
             if done:
